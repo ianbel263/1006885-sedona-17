01205f4b194984550684b92e3da2716293d2bb31
Andrey Belyaev <ianbel263@gmail.com>
2021-12-21 08:17:09 +0300

    add build folder to repo

---
+++ js/script 3.js
@@ -0,0 +1,19 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.page-header__button');
+
+navMain.classList.remove('main-nav--nojs');
+navToggle.classList.remove('page-header__button--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    navToggle.classList.remove('page-header__button--menu-closed');
+    navToggle.classList.add('page-header__button--menu-opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    navToggle.classList.add('page-header__button--menu-closed');
+    navToggle.classList.remove('page-header__button--menu-opened');
+  }
+});
